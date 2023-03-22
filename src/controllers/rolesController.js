c6a2498c2999cf387c960322d26aa72fa411a3c4
Aimeana100 <aimeanathole@gmail.com>
2023-03-22 12:23:48 +0200

    re structure the role and permissions

--- src/controllers/rolesController.js
+++ src/controllers/rolesController.js
@@ -1,4 +1,4 @@
-import { Role,Permission, RolePermission } from "../models";
+import { Role, Permission, RolePermission } from "../models";
 
 const getAllRoles = async (req, res) => {
   const roles = await Role.findAll({});
@@ -6,73 +6,74 @@ const getAllRoles = async (req, res) => {
 };
 
 const getRole = async (req, res) => {
-
-  if(!req.params.id) return res.status(400).json({ status: "error", message: "Bad Request" });
-
-  const role = await Role.findByPk(req.params.id);
-  if (!role) {
-    return res
-      .status(204)
-      .json({ message: `Role with id  does not exist` });
+  if (!req.params.id)
+    return res.status(400).json({ status: "error", message: "Bad Request" });
+  if(isNaN(req.params.id)){
+    return res.status(400).json({ status: "error", message: `Id should be a number` });
   }
-  res.status(200).json(role);
+
+  try {
+    const role = await Role.findByPk(req.params.id);
+    if (!role) {
+      return res.status(204).json({ message: `Role with id  does not exist` });
+    }
+    res.status(200).json(role);
+  } catch (error) {
+    return res.status(500).json({status: `error`, message: `Error: ${error.message}`});
+  }
 };
 
 const createRole = async (req, res) => {
-  if (
-    !req.body.name
-  ) {
+  if (!req.body.name) {
     return res
       .status(400)
       .json({ message: "Please provide all required information" });
   }
-  req.body['display_name'] = !req.body?.display_name ?  req.body.name : req.body['display_name'];
+  req.body["display_name"] = !req.body?.display_name
+    ? req.body.name
+    : req.body["display_name"];
 
-  const role = await Role.create(req.body);
+  try {
+    const role = await Role.create(req.body);
 
-
-  Object.keys(req.body).forEach(async (key, val) => {
-    let permissions = {};
-    if (key.startsWith("permission_")) {
-      permissions.PermissionId = Number(key.split("_")[1]);
-      permissions.RoleId = role.id;
-
-      let pkg = await Permission.findByPk(Number(key.split("_")[1]));
-      if (pkg) {
-        await RolePermission.create(permissions);
-      }
-    }
-  });
-
-  return res.status(201).json({ message: "ok", role });
+    return res.status(201).json({ message: "ok", role });
+  } catch (error) {
+    return res.status(500).json({ status: "error", message: error.message });
+  }
 };
 
 const updateRole = async (req, res) => {
-  if(!req.body.id) return res.status(400).json({ status: "error", message: "Bad Request" });
+  if (!req.body.id)
+    return res.status(400).json({ status: "error", message: "Bad Request" });
 
   const role = await Role.findByPk(req.body.id);
 
-  if(!role) {
+  if (!role) {
     return res.status(204).json({ message: `Role with id  does not found` });
   }
 
-  role.set(req.body)
+  role.set(req.body);
   await role.save();
 
-  return res.status(200).json({ status: "ok", message: "Role updated successfully",  role });
-}
+  return res
+    .status(200)
+    .json({ status: "ok", message: "Role updated successfully", role });
+};
 
 const deleteRole = async (req, res) => {
-  if(!req.params.id) return res.status(400).json({ status: "error", message: "Bad Request" });
+  if (!req.params.id)
+    return res.status(400).json({ status: "error", message: "Bad Request" });
 
   const role = await Role.findByPk(req.params.id);
 
-  if(!role) {
+  if (!role) {
     return res.status(204).json({ message: `Role with id  does not found` });
   }
   await role.destroy();
-  return res.status(200).json({ status: "ok", message: "Role deleted successfully" });
-}
+  return res
+    .status(200)
+    .json({ status: "ok", message: "Role deleted successfully" });
+};
 export default {
   getAllRoles,
   getRole,
