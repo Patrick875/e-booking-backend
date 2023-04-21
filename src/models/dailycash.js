c6a5825791df9fd97d22b64465e217c5e68a30a9
Aimeana100 <aimeanathole@gmail.com>
2023-04-21 14:40:45 +0200

    fixd

--- src/models/dailycash.js
+++ src/models/dailycash.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.belongsTo( models.User, { foreignKey : 'carriedBy' } )
-      this.hasOne( models.DailyMoneyDetail, { foreignKey : 'dailysalesId'})
+      this.hasMany( models.DailyMoneyDetail, { foreignKey : 'dailysalesId'})
     }
   }
   DailyMoney.init({
