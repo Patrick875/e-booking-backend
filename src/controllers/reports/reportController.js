061cb30c000d48214efe5b71b0e045ab2ebe132d
Aimeana100 <aimeanathole@gmail.com>
2023-05-01 13:45:08 +0200

    add reservations reports

--- src/controllers/reports/reportController.js
+++ src/controllers/reports/reportController.js
@@ -1,102 +1,74 @@
-import { Reservation , PetitStockSale, db } from '../../models'
+import { Reservation, PetitStockSale, db } from "../../models";
 import { asyncWrapper } from "../../utils/handlingTryCatchBlocks";
-import { Sequelize, Op } from 'sequelize';
-
-const config = require('../../config/config.js');
-const env = process.env.NODE_ENV;
-
-let sequelize;
-
-if (config[env].url) {
-  sequelize = new Sequelize(config[env].url);
-} else {
-  sequelize = new Sequelize(process.env.DEV_DATABASE_URL);
-}
+import { Sequelize, Op } from "sequelize";
 
 
+const yearlyReservation = asyncWrapper(async (req, res) => {
+  const year = req.params.year || new Date().getFullYear();
 
-// function to calculate the total amount for a given month and year
-const calculateMonthlyAmount = async (year, month) => {
-  const result = await Reservation.findAll({
-    attributes: [
-      [sequelize.fn('sum', sequelize.col('amount.RWF')), 'RWF_total'],
-    ],
+  // Controller function to handle requests for yearly reservation data
+
+  // Retrieve reservation data for the year
+  const reservations = await Reservation.findAll({
     where: {
-      [Op.and]: [
-        sequelize.where(sequelize.fn('year', sequelize.col('checkIn')), year),
-        sequelize.where(sequelize.fn('month', sequelize.col('checkIn')), month)
-      ]
-    }
+      checkIn: {
+        [Sequelize.Op.between]: [`${year}-01-01`, `${year}-12-31`],
+      },
+    },
+    attributes: ["id", "roomNumber", "checkIn", "checkOut", "amount"],
+    order: ["checkIn"],
   });
 
-  return result[0];
-};
-
-
-// function to calculate the monthly amounts for a given year
-const calculateYearlyAmounts = async (year) => {
-  const monthlyAmounts = [];
+  // Group reservation data by month
+  const reservationDataByMonth = {};
   for (let month = 1; month <= 12; month++) {
-    const monthlyAmount = await calculateMonthlyAmount(year, month);
-    monthlyAmounts.push(monthlyAmount);
+    reservationDataByMonth[month] = [];
+    const reservationsInMonth = reservations.filter(
+      (reservation) => reservation.checkIn.getMonth() + 1 === month
+    );
+    for (let reservation of reservationsInMonth) {
+      reservationDataByMonth[month].push({
+        id: reservation.id,
+        roomNumber: reservation.roomNumber,
+        checkIn: reservation.checkIn,
+        checkOut: reservation.checkOut,
+        amount: reservation.amount,
+      });
+    }
   }
-  return monthlyAmounts;
-};
 
+  // Build response object
+  const response = {
+    year: year,
+    reservationDataByMonth: reservationDataByMonth,
+  };
 
-
-const yearlyReservation = asyncWrapper( async ( req, res) => {
-
-const year = req.params.year || new Date().getFullYear();
-
-// const results = await Reservation.findAll({
-//   attributes: [
-//     [Sequelize.fn('date_trunc', 'month', Sequelize.col('createdAt')), 'month'],
-//     [Sequelize.fn('SUM', Sequelize.cast(Sequelize.col('amount->>RWF'), 'numeric')), 'rwf_total'],
-//     [Sequelize.fn('SUM', Sequelize.cast(Sequelize.col('amount->>USD'), 'numeric')), 'usd_total']
-//   ],
-//   where: Sequelize.where(
-//     Sequelize.fn('date_part', 'year', Sequelize.col('createdAt')),
-//     year
-//   ),
-//   group: [Sequelize.fn('date_trunc', 'month', Sequelize.col('createdAt'))],
-//   order: [Sequelize.fn('date_trunc', 'month', Sequelize.col('createdAt'))]
-// });
-
-
-
-const results = await calculateYearlyAmounts(year);
-
-// res.json({
-//   status: 'success',
-//   year: year,
-//   yearlyAmounts: results
-// });
-
-console.log(results);
-
-return res.status(200).json({status: 'success', data: results});
-})
-
-const yearlySales = asyncWrapper( async(req , res) => {
-
-  const year = req.params.year || new Date().getFullYear();
-  
-const results = await PetitStockSale.findAll({
-  attributes: [
-    [Sequelize.fn('date_trunc', 'month', Sequelize.col('createdAt')), 'month'],
-    [Sequelize.fn('SUM', Sequelize.cast(Sequelize.col('amount'), 'numeric')), 'rwf_total'],
-  ],
-  where: Sequelize.where(
-    Sequelize.fn('date_part', 'year', Sequelize.col('createdAt')),
-    year
-  ),
-  group: [Sequelize.fn('date_trunc', 'month', Sequelize.col('createdAt'))],
-  order: [Sequelize.fn('date_trunc', 'month', Sequelize.col('createdAt'))]
+  return res.status(200).json({ status: "success", data: response });
 });
 
-return res.status(200).json({status: 'success', data: results});
-    
-})
+const yearlySales = asyncWrapper(async (req, res) => {
+  const year = req.params.year || new Date().getFullYear();
 
-export default { yearlyReservation , yearlySales}
+  const results = await PetitStockSale.findAll({
+    attributes: [
+      [
+        Sequelize.fn("date_trunc", "month", Sequelize.col("createdAt")),
+        "month",
+      ],
+      [
+        Sequelize.fn("SUM", Sequelize.cast(Sequelize.col("amount"), "numeric")),
+        "rwf_total",
+      ],
+    ],
+    where: Sequelize.where(
+      Sequelize.fn("date_part", "year", Sequelize.col("createdAt")),
+      year
+    ),
+    group: [Sequelize.fn("date_trunc", "month", Sequelize.col("createdAt"))],
+    order: [Sequelize.fn("date_trunc", "month", Sequelize.col("createdAt"))],
+  });
+
+  return res.status(200).json({ status: "success", data: results });
+});
+
+export default { yearlyReservation, yearlySales };
